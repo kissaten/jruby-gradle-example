2c30eb905d3bf4e266bd2169c10d4364d2ff8a6f
Joe Kutner <jpkutner@gmail.com>
2015-07-22 13:00:10 -0500

    Added cowsay gem

--- entrypoint.rb
+++ entrypoint.rb
@@ -1,10 +1,11 @@
 require 'webrick'
+require 'cowsay'
 
 root = File.expand_path '~/public_html'
 server = WEBrick::HTTPServer.new :Port => (ENV['PORT'] || 8000), :DocumentRoot => root
 
 server.mount_proc '/' do |req, res|
-  res.body = 'Hello, world!'
+  res.body = "<pre>#{Cowsay.say("Hello")}</pre>"
 end
 
 trap 'INT' do server.shutdown end
