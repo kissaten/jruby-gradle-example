247c6fbab2975e5868fc058005fab7b068a410ff
Joe Kutner <jpkutner@gmail.com>
2015-07-22 12:42:56 -0500

    Added port env

--- entrypoint.rb
+++ entrypoint.rb
@@ -1,7 +1,7 @@
 require 'webrick'
 
 root = File.expand_path '~/public_html'
-server = WEBrick::HTTPServer.new :Port => 8000, :DocumentRoot => root
+server = WEBrick::HTTPServer.new :Port => (ENV['PORT'] || 8000), :DocumentRoot => root
 
 server.mount_proc '/' do |req, res|
   res.body = 'Hello, world!'
